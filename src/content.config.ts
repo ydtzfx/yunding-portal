e0a09dd0f596d0ad44eeb7e8bb240e1e26a9700e
Yunding Investment <134386970+ydtzfx@users.noreply.github.com>
2026-09-20 04:57:43 +0200

    chore(astro): migrate content schema to astro/zod

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,4 +1,5 @@
-import { defineCollection, z } from 'astro:content';
+import { defineCollection } from 'astro:content';
+import { z } from 'astro/zod';
 import { glob } from 'astro/loaders';
 
 const citationDate = z.union([z.string(), z.date(), z.number()]).transform((value) => {
